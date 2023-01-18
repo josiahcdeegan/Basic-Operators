3d0d728adb6a57baea8d8afae797ba10e3ad4940
josiahcdeegan <114314800+josiahcdeegan@users.noreply.github.com>
2023-01-18 15:18:09 -0500

    Okay

---
+++ src/basic_operators/Basic_Operators.java
@@ -0,0 +1,11 @@
+package basic_operators;
+public class Basic_Operators{
+
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+	
+		int r = 287;
+		int s = 7;
+		System.out.println(r % s);
+}}
